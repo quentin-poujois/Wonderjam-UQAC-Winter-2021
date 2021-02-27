6acfc4eca2e449a77723ef9a15405ab80480d6d1
M5Altaïr <guillaume.vinet.38@gmail.com>
2021-02-26 22:51:40 -0500

    Add navmesh agent and navigation + MonsterController script

---
+++ Assets/MonsterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+
+    NavMeshAgent navMesh;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.SetDestination(Player.transform.position);
+    }
+}
